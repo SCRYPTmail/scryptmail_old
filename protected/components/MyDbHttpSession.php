833be73df3bb2c8660587fa140bf2d786367fa20
vSaKv <sergyk17@yahoo.com>
2015-02-22 14:07:21 -0800

    login error fix, new Q&A

--- protected/components/MyDbHttpSession.php
+++ protected/components/MyDbHttpSession.php
@@ -11,19 +11,25 @@ class MyDbHttpSession extends CDbHttpSession
 	{
 		$db = $this->getDbConnection();
 		$db->setActive(true);
-		$db->createCommand()->update(
+
+		//print_r(Yii::app()->db->createCommand("UPDATE ".$this->sessionTableName." SET userId=:userId, id=:id WHERE id=:id"));
+		if(Yii::app()->db->createCommand("UPDATE ".$this->sessionTableName." SET userId=:userId, id=:id WHERE id=:id")->execute(array(':userId' => $userId,':id'=>session_id()))){
+		}else{
+			Yii::app()->db->createCommand("INSERT INTO ".$this->sessionTableName." (userId,id) VALUES(:userId,:id)")->execute(array(':userId' => $userId,':id'=>session_id()));
+		}
+
+		/*$db->createCommand()->update(
 			$this->sessionTableName,
-			array('userId' => $userId), // I asume you added a column 'userId' to your session table
+			array('userId' => $userId),
 			'id=:id',
 			array(':id' => session_id())
-		);
+		);*/
 	}
 
 	public function deleteOldUserSessions($userId)
 	{
 		$db = $this->getDbConnection();
 		$db->setActive(true);
-
 		Yii::app()->db->createCommand("DELETE FROM ".$this->sessionTableName." WHERE userId=:userId")->execute(array(':userId' => $userId));
 	}
 }
