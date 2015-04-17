f1bf6e7d478fb397c5a7d7db69c1f6a4fec1370d
vSaKv <sergei@scryptmail.com>
2015-04-17 10:18:42 -0700

    Update genFunctions.js

--- js/genFunctions.js
+++ js/genFunctions.js
@@ -1033,7 +1033,6 @@ function fileSelection(object, container) {
 
 
 function IsEmail(email) {
-	functionTracer='IsEmail';
 	var regex = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_<`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z >]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i;
 
 	return regex.test(email);
@@ -3392,10 +3391,11 @@ function renameCustomFolder(name,id){
 						
 						checkFolders();
 						displayFolder();
-						$(this).dialog("close");
-						$('#newFolder').val('');
+					getDataFromFolder(id);
 						}
 						
+						$(this).dialog("close");
+						$('#newFolder').val('');
 						
 
 
@@ -3465,7 +3465,6 @@ if(customMessageIds(folder['Custom'][id]).length>0){
 }
 
 function SHA1(text){
-	functionTracer='SHA1';
 	var md = forge.md.sha1.create();
 	md.update(text,'utf8');
 	return md.digest().toHex()
